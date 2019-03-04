7bf5299e2455bcaa10718f926a1db8e9dc46238d
Светлана Димова <svetlana.dmitrenko@gmail.com>
2019-03-04 14:22:39 +0200

    Add project files.

---
+++ Lesson 4 -home work/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4__home_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
